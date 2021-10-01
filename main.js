760256775691ae56b49d3d54ee5dc8dba3ddc6d5
tristan girard <tristan.girard@live.ca>
2021-10-01 14:34:04 -0400

    header1

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import './style.scss'
+import * as Popper from '@popperjs/core';
+import './style.scss';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
