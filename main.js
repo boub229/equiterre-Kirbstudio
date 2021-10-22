11743750e672b63da1281b3c8d056fcbcda698f8
Louis-Philippe Gravel <70409865+Louis1er@users.noreply.github.com>
2021-10-22 14:40:23 -0400

    hover_hamburger

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import Swiper from 'swiper';
+import { gsap } from "gsap";
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 SwiperCore.use([Navigation, Pagination]);
+
 
 var Swipes = new Swiper('.swiper-container', {
     loop: true,
@@ -20,3 +22,21 @@ var Swipes = new Swiper('.swiper-container', {
     },
 });
 
+
+gsap.from('.navbar-toggler-icon', 
+        {x: 0, 
+         duration: 5, 
+         });
+
+
+const hamburger = document.querySelector(".navbar-toggler");
+const hover = gsap.to(hamburger, {
+  y:'-20%',
+  duration: 0.5,
+  paused: true,
+  ease: "ease-in-out"
+});
+
+
+hamburger.addEventListener("mouseenter", () => hover.play());
+hamburger.addEventListener("mouseleave", () => hover.reverse());
