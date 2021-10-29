a06edfa2e1b7fe137b22ace46a43ea14489113b1
tristan girard <70409109+boub229@users.noreply.github.com>
2021-10-29 16:54:19 -0400

    debut_wp

--- wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,4 +1,7 @@
-<section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
-</section>
+<div class="error">
+        <div class="err_style">
+            ERROR 404
+          </div>
+             
+<a class='arbret1' href="https://imgur.com/MzV5UQl"><img src="https://i.imgur.com/MzV5UQl.png" title="source: imgur.com" /></a>
+      <a class='arbret2'href="https://imgur.com/7MRLzsD"><img src="https://i.imgur.com/7MRLzsD.png" title="source: imgur.com" /></a>
