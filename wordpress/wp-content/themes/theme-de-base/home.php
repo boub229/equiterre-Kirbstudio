6cde656af4a878a498d620770418b30f24b4c57e
alxi21 <70409941+alxi21@users.noreply.github.com>
2021-11-08 16:48:22 -0500

    accueil wordpress

--- wordpress/wp-content/themes/theme-de-base/home.php
+++ wordpress/wp-content/themes/theme-de-base/home.php
@@ -1,6 +1,7 @@
 <?php 
 /**
  * 	Template Name: Accueil
+ *  Template post type: image_service
  * 	Identique à page, mais avec une barre latérale
  */
 
@@ -19,14 +20,14 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 		<?php endif; ?>
 		
 		<?php the_content(); // Contenu principal de la page ?>
-    <section>
 
+
+
+    <section>
       <div class="swiper-container" id='index'>
-    
-       
         <div class="swiper-wrapper">
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel1_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel1_vector.png">
               <div class="opacity"></div>
                 <h2 class="titre_articles_noir">Événement-bénéfice 2021</h2>
                 <p class="description_articles_noir"> Équiterre vous invite à participer à son événement-bénéfice 2021 qui <br> portera sur le thème « Économie circulaire : les emplois de l'avenir ».</p>
@@ -34,7 +35,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
             
             <div class="swiper-slide"> 
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel2_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel2_vector.png">
               <div class="opacity"></div>
                 <h2 class="titre_articles_noir">Nouvelle pétition : exigeons des <br>biens durables et réparables !</h2>
                 <p class="description_articles_noir"> Un ordinateur qui rend l’âme après un an, un écran de cellulaire qui se fissure au moindre choc, une laveuse<br> qui n’essore plus… et tout un casse-tête pour celles et ceux qui ont la ténacité de vouloir les faire réparer! </p>
@@ -42,7 +43,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
             
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel3_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel3_vector.png">
                 <div class="opacity"></div>
                 <h2 class="titre_articles_gros">Le pouvoir des sols : une feuille de route au profit <br> des agriculteurs et de la résilience climatique</h2>
                 <p class="description_articles">Dans un rapport dévoilé le 1er mars 2021, Équiterre et son partenaire, la Fondation Greenbelt, démontrent comment des <br>sols en santé peuvent permettre aux fermes canadiennes de jouer un rôle central pour affronter la crise climatique et...</p>
@@ -50,7 +51,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
             
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel4_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel4_vector.png">
               <div class="opacity"></div>
               <h2 class="titre_articles">Dossier complet : L’enjeu du dézonage agricole au Québec</h2>
               <p class="description_articles">Les terres agricoles québécoises sont des ressources rares et précieuses. Pourtant, la pression à laquelle elles sont assujetties est alarmante. <br>Malgré la Loi sur la Protection du territoire et des activités agricoles, adoptée il y a 40 ans, nos terres...</p>
@@ -58,7 +59,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
             
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel5_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel5_vector.png">
               <div class="opacity"></div>
               <h2 class="titre_articles_noir">Des outils par et pour les agriculteurs-trices!</h2>
               <p class="description_articles_noir">Dans le cadre de la Semaine canadienne de l’environnement, Équiterre et Coordination services-conseil, avec le soutien financier du ministère de <br>l’Agriculture, des Pêcheries et de l’Alimentation du Québec, lancent les Vitrines en santé et...</p>
@@ -66,7 +67,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
 
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel6_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel6_vector.png">
               <div class="opacity"></div>
               <h2 class="titre_articles_gros">Comprendre la hausse des camions légers <br> au Canada afin de renverser la tendance</h2>
               <p class="description_articles">Alors que le Canada est engagé dans l’Accord de Paris et s’affiche comme un leader climatique à l’international, <br>le pays demeure l’un des plus grands pollueurs par habitant.e dans le monde et n’est pas en voie...</p>
@@ -74,7 +75,7 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             </div>
 
             <div class="swiper-slide">
-              <img class="img-swiper" src="sources/médias/Accueil/carrousel7_vector.png">
+              <img class="img-swiper" src="<?php echo get_template_directory_uri();?>/img/Accueil/carrousel7_vector.png">
               <div class="opacity"></div>
               <h2 class="titre_articles_noir">Rapport annuel d’Équiterre 2020</h2>
               <p class="description_articles_noir">2020 nous a beaucoup appris. Elle a changé nos perspectives, ravisé nos priorités et nous a fait comprendre que, oui, des changements<br> profonds et systémiques sont possibles. Ils sont même plus que possibles, ils sont nécessaires.</p>
@@ -83,20 +84,34 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
             
         </div>
 
-        
-          
-       
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
         <div class="swiper-pagination"></div>
-        
-        
-       
-        
+   
     </div>
     </section>
 
-		<section>
+  <section>
+  <div class="bloc_services">
+      <div class="service1">
+        <div class="color-1">
+        </div>
+        <p class="text_cartes">École ouverte</p>
+      </div>
+
+      <div class="service2">
+        <div class="color-2">
+        </div>
+        <p class="text_cartes">Paniers bio</p>
+      </div>
+      <div class="service3">
+        <div class="color-3">
+        </div>
+        <p class="text_cartes">Épluche ta ville pour manger local</p>
+      </div>
+    </div>
+  </section>
+  <section>
     <h1>Témoignage</h1>
     <div class="container-fluid">
       <div class="row">
@@ -112,9 +127,48 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
       </div>
     </div>
   </section>
+  <section>
+  <h1>Nouveautés</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-sm-12 text-center">
+        <img class="img-nouvelles" src="sources/médias/détail_nouvelles/nouvelle1.jpg">
+        <h2 class="text-nouvelles">Conférence vedette de 2019 - Obsolescence : quel rôle pour le consommateur</h2>
+        <button class="button-savoir-plus">En savoir plus</button>
+      </div>
+      <div class="col-lg-4 col-sm-12 text-center">
+        <img class="img-nouvelles" src="sources/médias/détail_nouvelles/nouvelle2.jpg">
+        <h2 class="text-nouvelles">Les péripéties d'un fermier de famille : un livre à savourer dès maintenant</h2>
+        <button class="button-savoir-plus">En savoir plus</button>
+      </div>
+      <div class="col-lg-4 col-sm-12 text-center">
+        <img class="img-nouvelles" src="sources/médias/détail_nouvelles/nouvelle3.png">
+        <h2 class="text-nouvelles">Des fermiers d'excellence dans notre réseau!<br> ㅤ</h2>
+        <button class="button-savoir-plus">En savoir plus</button>
+        
+      </div>
 
-  
-	</article>
+    </div>
+  </div>
+</section>
+<section>
+  <h1>Dons</h1>
+  <div class="container-don">
+    <div class="btn-don">
+      <div class="text-don">Faites un don!</div>
+    </div>
+  </div>
+</section>
+</article>
+<style>
+    .img-témoignages img{
+      width: 100%;
+      height: 100%;
+      text-align: center;
+    }
+
+</style>
+
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
--- wordpress/wp-content/themes/theme-de-base/news.php
+++ wordpress/wp-content/themes/theme-de-base/news.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * 	Template Name: À propos
+ * 	Template Name: Nouvelles
  * 	Identique à page, mais avec une barre latérale
  */
 
@@ -19,6 +19,76 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 		<?php endif; ?>
 		
 		<?php the_content(); // Contenu principal de la page ?>
+
+
+<section id='nouvelles'>
+  <div class="hero">
+      <div class="fruit">
+          <div class="image_nouvelle">
+          <div class="opacity"></div>
+            <div class="titre_nouvelle">
+              <h2>Nouvelles</h2>
+            </div>
+            <img src="/sources/médias/détail_nouvelles/fruit.jpg" alt="">
+          </div>
+      </div>
+  </div>
+
+<div class='cartes_nouvelles'>
+<div class='cartes'>
+  <div class="opacity_détail1"></div>
+  <div class="titre_cartes1"><h2>
+    CONFÉRENCE VEDETTE DE
+    2019 - OBSOLESCENCE :
+    QUEL RÔLE POUR LE
+    CONSOMMATEUR?</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle1.jpg"  class='img-nouvelles-détail' alt="">
+  
+</div>
+<div class='cartes'>
+  <div class="opacity_détail"></div>
+  <div class="titre_cartes"><h2>
+    FAITES L'EXPÉRIENCE DE LA SEULE COURSE 100% ÉCORESPONSABLE!</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle2.jpg"  class='img-nouvelles-détail' alt="">
+</div>
+<div class='cartes'>
+  <div class="opacity_détail"></div>
+  <div class="titre_cartes"><h2>
+    LA CRISE DES PESTICIDES AU QUÉBEC : À QUAND LA TRANSPARENCE?</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle3.png"  class='img-nouvelles-détail' alt="">
+</div>
+<div class='cartes'>
+  <div class="opacity_détail"></div>
+  <div class="titre_cartes"><h2>
+    4 ACTIONS POUR SAUVER #NOTREHIVER !</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle4-1.jpg"  class='img-nouvelles-détail' alt="">
+</div>
+<div class='cartes'>
+  <div class="opacity_détail"></div>
+  <div class="titre_cartes"><h2>   
+    GESTE DU MOIS DE FÉVRIER : CE 14 FÉVRIER... BOUDONS LA ROSE!</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle5-1.jpg"  class='img-nouvelles-détail' alt="">
+</div>
+<div class='cartes'>
+  <div class="opacity_détail"></div>
+  <div class="titre_cartes"><h2>
+    VULGARISATION SCIENTIFIQUE, CHANGEMENTS CLIMATIQUES ET ALIMENTATION EN ARCTIQUE : LA 9E BOURSE LAURE WARIDEL DÉCERNÉE À MARIANNE FALARDEAU-CÔTÉ</h2>
+  </div>
+  <img src="/sources/médias/détail_nouvelles/nouvelle6-1.jpg"  class='img-nouvelles-détail' alt="">
+</div>
+<button class='bouton_nouvelles'>Voir plus de nouvelles</button>
+</div>
+
+</section>
+
+
+
+
 	</article>
 <?php endwhile; // Fermeture de la boucle
 
