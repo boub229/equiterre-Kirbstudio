8a5ebfb7c2d87f7e319581062fad320037e55a0b
Louis-Philippe Gravel <70409865+Louis1er@users.noreply.github.com>
2021-12-03 10:48:23 -0500

    style_page404

--- wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -6,8 +6,8 @@
             ERROR 404
           </div>
              
-<a class='arbret1' href="https://imgur.com/MzV5UQl"><img src="https://i.imgur.com/MzV5UQl.png" title="source: imgur.com" /></a>
-      <a class='arbret2'href="https://imgur.com/7MRLzsD"><img src="https://i.imgur.com/7MRLzsD.png" title="source: imgur.com" /></a>
+<a class='arbret1'><img src="https://i.imgur.com/MzV5UQl.png" title="source: imgur.com" /></a>
+      <a class='arbret2'><img src="https://i.imgur.com/7MRLzsD.png" title="source: imgur.com" /></a>
 
 
 
@@ -33,32 +33,33 @@ html, body{
 .error{
   text-align:center;
   height:100%;
-  
   display: flex;
   justify-content: center;
   align-items: center;
   
 }
 
-img{
-
-  width: 50%;
-}
 .arbret1{
   overflow: hidden;
   position:absolute;
- 
   width: 100%;
   z-index: 2;
- 
+
 }
 
 .arbret2{
-  
   width: 100%;
   z-index: 3;
- 
 }
+
+.arbret1 img{
+  width: 50%;
+}
+
+.arbret2 img{
+  width: 50%;
+}
+
 </style>
 
 <script>
